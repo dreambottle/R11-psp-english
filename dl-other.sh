093676f0767a4e06209945f519dda06c065a441a
Nico Juice <primenico@gmail.com>
2016-02-15 23:20:12 +0200

    added text, downloaded from the website

---
+++ dl-other.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+TEXTDIR=www-other
+mkdir -p $TEXTDIR
+
+LIST=`wget 'http://tlwiki.org/index.php?title=Remember11_-_the_age_of_infinity' -O - |\
+      perl -lne '/<b><a href="[^" ]+title=R11:(Shortcut|ED_Movie|OP_Movie|Songs|init.txt.B)/ and print $1'`
+for i in $LIST ; do
+    wget 'http://tlwiki.org/index.php?title=R11:'$i -O - |\
+    perl -p0e 'm#<pre>(.*)</pre>#s or die;$_=$1; %h=(nbsp=>" ", quot=>"\"", lt=>"<", gt=>">"); s/\&(\w+);/defined($h{$1})||die$&; $h{$1}/ge;' > $TEXTDIR/$i
+done
