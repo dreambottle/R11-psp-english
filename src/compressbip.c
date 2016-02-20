740f2acc55601dfc198d4af4a5d3fccdb0290dcf
Nico Juice <primenico@gmail.com>
2016-02-20 05:43:09 +0200

    minor edits

--- src/compressbip.c
+++ src/compressbip.c
@@ -5,12 +5,19 @@ int main(int argc, char **argv) {
     assert2(argc==3, "usage: %s in out.bip\n", argv[0]);
     off_t in_size;
     uint8_t *in = mmap_file(argv[1], &in_size);
-	printf("Original size: %d bytes\n", in_size);
+	printf("Original size: %d bytes\n", (int)in_size);
 	off_t out_size = (in_size*9+7)/8 + 4; //worst case
-    uint8_t *out = malloc(out_size);
+    uint8_t *out;
+
+	out = malloc(out_size);
     *(int*)out = in_size;
-	/* 
+	uint8_t *out_end = compress_lzss(out+4, out_size-4, in, in_size);
+	off_t compressed_size = out_end - out;
+	
+	/*
 	// this code doesn't do any compression
+	out = malloc(out_size+18);
+	*(int*)out = in_size;
 	int i=0, o=4;
     while(i<in_size) {
         if(!(i&7))
@@ -18,11 +25,10 @@ int main(int argc, char **argv) {
         out[o++] = in[i++];
     }
     assert(o == out_size);
-	compress_lzss*/
-	uint8_t *out_end = compress_lzss(out+4, out_size-4, in, in_size);
-	off_t compressed_size = out_end - out;
-	printf("Compressed size: %d bytes\n", compressed_size, in_size);
-
+	off_t compressed_size = o;
+	*/
+	
+	printf("Compressed size: %d bytes\n", (int)compressed_size);
     write_file(argv[2], out, compressed_size);
 	free(out);
     return 0;
--- text/dl-other.sh
+++ text/dl-other.sh
@@ -3,7 +3,7 @@ TEXTDIR=www-other
 mkdir -p $TEXTDIR
 
 LIST=`wget 'http://tlwiki.org/index.php?title=Remember11_-_the_age_of_infinity' -O - |\
-      perl -lne '/<b><a href="[^" ]+title=R11:(Shortcut|ED_Movie|OP_Movie|Songs|init.txt.B)/ and print $1'`
+      perl -lne '/<b><a href="[^" ]+title=R11:(Shortcut|ED_Movie|OP_Movie|init.txt.B)/ and print $1'`
 for i in $LIST ; do
     wget 'http://tlwiki.org/index.php?title=R11:'$i -O - |\
     perl -p0e 'm#<pre>(.*)</pre>#s or die;$_=$1; %h=(nbsp=>" ", quot=>"\"", lt=>"<", gt=>">"); s/\&(\w+);/defined($h{$1})||die$&; $h{$1}/ge;' > $TEXTDIR/$i
