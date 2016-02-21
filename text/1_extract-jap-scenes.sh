46f3042923df2325fee47e0ef566c39f30bc7bbb
Nico Juice <primenico@gmail.com>
2016-02-21 22:08:23 +0200

    script for combining extracted and translated lines

--- text/1_extract-jap-scenes.sh
+++ text/1_extract-jap-scenes.sh
@@ -6,7 +6,9 @@ mkdir -p mac-ja-psp/
 mkdir -p mac-combined-psp/
 for i in ../mac/[A-Z0-9]*_[0-9]*.SCN ; do
 	f=`basename $i .SCN`
+	echo $f
+	# process translation files
 	#../extract_scene_text ../mac/$f.SCN mac-ja-psp/$f.txt || exit 1;
-	#combine 
+	# combine with my format
 	./insert_en_lines.py mac-ja-psp/$f.txt mac-en-only/$f.txt mac-combined-psp/$f.txt || exit 1;
 done
---
+++ text/insert_en_lines.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+import mmap
+
+
+def main():
+  if len(sys.argv) != 4:
+    sys.exit("usage: %s jap.extracted.txt en.lines.txt out.combined.txt" % sys.argv[0]);
+  else:
+    jap = sys.argv[1];
+    en  = sys.argv[2];
+    out = sys.argv[3];
+
+  #, encoding="SHIFT-JIS"
+  jap_f = open(jap, "rb");
+  en_f  = open(en,  "rb");
+  
+  jap_lines = jap_f.readlines();
+  en_lines  = en_f.readlines();
+
+  jap_head = jap_lines[:3];
+  jap_payload = jap_lines[3:];
+  jap_entries_count = (len(jap_payload)) // 3;
+  if len(en_lines) != jap_entries_count:
+    print("Warning: entry count mismatch!");
+    print("Lines jap: %d(effective:%d), en: %d." % (len(jap_lines), jap_entries_count, len(en_lines)) );
+
+  out_f = open(out, "wb");
+  for i in range(len(jap_head)):
+    out_f.write(jap_head[i]);
+
+  for i in range(len(en_lines)):
+    if i < jap_entries_count:
+      out_f.write(jap_payload[i*3]);
+      out_f.write(jap_payload[i*3+1]);
+    if i < len(en_lines):
+      out_f.write(en_lines[i]);
+  out_f.close();
+  jap_f.close();
+  en_f.close();
+
+
+if __name__ == '__main__':
+  main();
