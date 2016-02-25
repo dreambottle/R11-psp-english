470c43789fec5015f4e74395ddc15ea7a3715139
Nico Juice <primenico@gmail.com>
2016-02-26 01:48:44 +0200

    added -xa switch so that resulting iso works on "real" psp

--- pack-iso.sh
+++ pack-iso.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-# Files produced by mkisofs/genisoimage fail to start on psp, but are ok for
-# an emulator, or for testing via usb.
 
-# For a release version which will launch on psp, use umdgen, or write your own tool :P .
-
-mkisofs -iso-level 4 -A "PSP GAME" -V "R11" -sysid "PSP GAME" -P "dreambottle" -o iso/r11-repacked.iso iso_extracted/
+mkisofs -iso-level 4 -xa -A "PSP GAME" -V "R11" -sysid "PSP GAME" -volset "" -p "" -P "" -o iso/r11-repacked.iso iso_extracted/
