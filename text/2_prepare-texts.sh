50278f372e4c84f1bd3d7285c07a1b11cd83c2b3
Niko Juice <primenico@gmail.com>
2016-07-13 18:47:11 +0300

    parallellized english scene text merging

--- text/2_prepare-texts.sh
+++ text/2_prepare-texts.sh
@@ -11,17 +11,33 @@
 # Dependencies: Perl File::Slurp module.
 # > cpan File::Slurp
 
+merge_translation () {
+	# process special characters and remove unimportant lines
+    ./extract_en.pl chapters-psp/$1.txt > mac-en-only/$1.txt || exit 1;
+	# combine with my format
+	./merge-scene-lines.py mac-ja-psp/$1.txt mac-en-only/$1.txt mac-combined-psp/$1.txt || exit 1;
+}
+
+START=$(date +%s.%N)
 mkdir -p mac-en-only/
+WAITPIDS=""
 for i in chapters-psp/[A-Z0-9]*_[0-9]*.txt ; do
 	f=`basename $i .txt`
 	echo Processing translation: $i
-	# process special characters and remove unimportant lines
-    ./extract_en.pl chapters-psp/$f.txt > mac-en-only/$f.txt || exit 1;
+	# # process special characters and remove unimportant lines
+    # ./extract_en.pl chapters-psp/$f.txt > mac-en-only/$f.txt || exit 1;
 	
-	# combine with my format
-	./merge-scene-lines.py mac-ja-psp/$f.txt mac-en-only/$f.txt mac-combined-psp/$f.txt || exit 1;
-
+	# # combine with my format
+	# ./merge-scene-lines.py mac-ja-psp/$f.txt mac-en-only/$f.txt mac-combined-psp/$f.txt || exit 1;
+	
+	merge_translation $f & WAITPIDS="$WAITPIDS $!"
 done
+
+echo "Waiting to finish..."
+wait $WAITPIDS &> /dev/null
+END=$(date +%s.%N)
+DIFF=$(echo "$END - $START" | bc)
+echo "Finished processing in: "$DIFF
 
 cd font
 # ./extract-font.py autotrim || exit 1;
