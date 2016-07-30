860f258ff37c04ca1a118a337b89253e948e66c0
Niko Juice <primenico@gmail.com>
2016-07-31 02:16:40 +0300

    small fix

--- text/1_extract-jap-scenes.sh
+++ text/1_extract-jap-scenes.sh
@@ -8,7 +8,7 @@ for i in ../mac/[A-Z0-9]*_[0-9]*.SCN ; do
 	f=`basename $i .SCN`
 	echo "Extracting scene $f text"
 	# process translation files
-	../bin/extract_scene_text ../mac/$f.SCN mac-ja-psp/$f.txt & WAITPIDS="$! WAITPIDS" #|| exit 1;
+	../bin/extract_scene_text ../mac/$f.SCN mac-ja-psp/$f.txt & WAITPIDS="$! $WAITPIDS" #|| exit 1;
 done
 wait $WAITPIDS &>/dev/null
 echo "Done."
