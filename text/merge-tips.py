51b99fac0990414a9d0d4a5f27ced57b5146fa5b
Niko Dzhus <primenico@gmail.com>
2018-09-01 03:28:37 +0300

    new file layout for tips, use PSP indexes, mod tips merge script

--- text/merge-tips.py
+++ text/merge-tips.py
@@ -9,14 +9,19 @@ from collections import deque
 
 debug = True
 
-tips_folderpath = "psp-tips/"
+tips_folderpath = "tips-psp/"
 tips_titles = "other-psp/Tips.titles.txt"
 tips_order = "other-psp/tips.order.psp.txt"
 mergewith = "other-psp/init.psp.txt"
-saveas = "other-psp/init.psp.txt"
-# saveas = "tips.init.psp.txt"
+# saveas = "other-psp/init.psp.txt"
+saveas = "tips.init.psp.txt"
 
-Tip = namedtuple("Tip", "i, filename, ipsp, jpname, enname, paragraphs")
+# Was used to convert old format of TIPS to a new one.
+# Only keeping the code for reference, it is now obsolete.
+convert2dir = None
+# convert2dir = "tips-psp2/"
+
+Tip = namedtuple("Tip", "i_psp, i_pc, filename, jpname, enname, jpparagraphs, paragraphs")
 
 def prepare_sjis_conv(str):
 #         %w|301C FF5E|, # WAVE DASH            => FULLWIDTH TILDE
@@ -46,33 +51,38 @@ def main():
   tipfiles = listdir(tips_folderpath)
   tipfiles.sort()
   if (len(titles) != len(tipfiles)):
-    exit("tips count mismatch")
+    exit("tips count mismatch; files: %s, titles: %s"%(len(tipfiles), len(titles)))
   
   orderjp = readlines_in_textfile(tips_order)
 
   # read all tip files
   if debug: print("Reading TIP files")
-  titles = deque(titles)
+  # titles = deque(titles)
   tips = []
   for i, t in enumerate(tipfiles):
     lines = readlines_in_textfile(tips_folderpath + t)
     
     # The second line in every tip file indicates the jp tip title. Finding its index.
-    tip_title = lines[1]
-    jpidxname = next(((i, t) for i, t in enumerate(orderjp) if tip_title == t), None) \
-            or next(((i, t) for i, t in enumerate(orderjp) if tip_title.startswith(t)))
+    tip_jp_title = lines[1]
+    jp_idx_name = next(((i, jp_t) for i, jp_t in enumerate(orderjp) if tip_jp_title == jp_t), None)
     
-    tip = Tip(i,
-              filename = t,
-              ipsp = jpidxname[0],
-              jpname = jpidxname[1],
-              enname = titles.popleft(),
+    translated_title = lines[2]
+
+    # 6-th line holds the index of the tip in the PC version
+    i_pc = int(lines[5][-3:])
+
+    tip = Tip(filename = t,
+              i_pc = i_pc,
+              i_psp = jp_idx_name[0],
+              jpname = jp_idx_name[1],
+              enname = translated_title,
+              jpparagraphs = [],
               paragraphs = [])
 
     if debug:
-      print("Processing TIP PC#%s (%s), PSP#%s: \"%s\"/\"%s\""%(i, t, tip.ipsp, tip.enname, tip.jpname))
+      print("Processing TIP PC#%s, PSP#%s, (%s): \"%s\"/\"%s\""%(i_pc, tip.i_psp, t, tip.enname, tip.jpname))
 
-    translation_start_index = lines.index("<pre>") + 1
+    translation_start_index = lines.index("#PARAGRAPHS_TRANSLATED:") + 1
     translated_lines = lines[translation_start_index:]
     translated_lines.append("")
     tip_paragraph = ""
@@ -98,16 +108,16 @@ def main():
 
     tips.append(tip)
 
-  tips = sorted(tips, key=lambda tip: tip.ipsp)
+  tips = sorted(tips, key=lambda tip: tip.i_psp)
   if debug:
     print("Analyzing overflows.")
     overflows = 0
 
     for tip in tips:
       for p_i, p in enumerate(tip.paragraphs):
-        if len(p) >= 480:
+        if len("".join(p.split("%N"))) > 480:
           overflows += 1
-          print('Buffer overflow! Tip PSP#%s (%s) "%s"/"%s", P#%s len: %s'%(tip.ipsp, tip.filename, tip.enname, tip.jpname, p_i+1, len(p)))
+          print('Buffer overflow! Tip PSP#%03d (%s) "%s"/"%s", P#%s len: %s'%(tip.i_psp, tip.filename, tip.enname, tip.jpname, p_i+1, len(p)))
           # print(p)
 
     print("Overflows found: %s"%(overflows))
@@ -118,23 +128,58 @@ def main():
   f_tips.close()
   
   # Hardcode. Change this if required
-  tipstart = 7343 #line, starting from 0
-  # tipsend = 8110
+  tips_start = 7343 #line, starting from 0
+  # tips_end = 8110
   
-  i = tipstart+1
+  # current line pointer
+  cur = tips_start
+  
   for tip in tips:
-    if debug: print(tip.ipsp, "(pc:%d)"%(tip.i+1), tip.enname, tip.jpname)
-    tips_inittxt_lines[i] = tip.enname.encode("shift_jis_2004") + b'\n'
+    if debug: print("Merging %03d"%(tip.i_psp), "(pc:%03d)"%(tip.i_pc), tip.enname, tip.jpname)
+    tips_inittxt_lines[cur + 1] = tip.enname.encode("shift_jis_2004") + b'\n'
 
-    if (not tips_inittxt_lines[i-1].endswith(tip.jpname.encode("shift_jis_2004")+b'\n') #, "backslashreplace"
-        and tip.ipsp!=37 and not 102>=tip.ipsp>=92):
-      print("Mismatch!", tip.jpname, "not in the one of", tips_inittxt_lines[i-1].decode("shift_jis_2004")) #, "replace"
+    if (not tips_inittxt_lines[cur].endswith(tip.jpname.encode("shift_jis_2004")+b'\n')):
+      print("Mismatch!", tip.jpname, "not found, instead got:", tips_inittxt_lines[cur].decode("shift_jis_2004"))
       break
-    
-    i += 2
-    for paragraph in tip.paragraphs:
-      tips_inittxt_lines[i] = prepare_sjis_conv(paragraph).encode("shift_jis_2004") + b'\n' #, "backslashreplace"
-      i += 2
+
+    # if convert2dir:
+    #   jp_title_line = tips_inittxt_lines[cur].decode("shift_jis_2004")
+    #   tip_start_addr = jp_title_line.split(";")[1]
+    #   jp_title = jp_title_line.split(";")[3].rstrip()
+
+    # move pointer to first paragraph
+    cur += 2
+    for p_i, paragraph in enumerate(tip.paragraphs):
+      tips_inittxt_lines[cur + 1] = prepare_sjis_conv(paragraph).encode("shift_jis_2004") + b'\n'
+      tip.jpparagraphs.append(tips_inittxt_lines[cur].decode("shift_jis_2004").rstrip())
+      cur += 2
+
+    # if convert2dir:
+    #   with open("%sTIP%03d.txt"%(convert2dir, tip.i_psp), "w") as newtip_out:
+    #     newtip_out.write("TIP%03d\n"%(tip.i_psp))
+    #     newtip_out.write("%s\n"%(jp_title))
+    #     newtip_out.write("%s\n"%(tip.enname))
+    #     newtip_out.write("paragraph_count:%d\n"%(len(tip.paragraphs)))
+    #     newtip_out.write("init_addr:%s\n"%(tip_start_addr))
+    #     newtip_out.write("pc_index:TIP_%03d\n"%(tip.i_pc))
+
+    #     newtip_out.write("\n")
+    #     newtip_out.write("#PARAGRAPHS_JP:\n")
+    #     for p_i, paragraph in enumerate(tip.jpparagraphs):
+    #       newtip_out.write("#JP#%s\n"%(p_i+1))
+    #       jp_paragraph_multiline = "%N\n".join(paragraph.split("%N"))
+    #       newtip_out.write("%s\n"%(jp_paragraph_multiline))
+    #       if not jp_paragraph_multiline.endswith("\n"): newtip_out.write("\n")
+
+    #     newtip_out.write("\n")
+    #     newtip_out.write("#PARAGRAPHS_TRANSLATED:\n")
+    #     for p_i, paragraph in enumerate(tip.paragraphs):
+    #       newtip_out.write("#EN#%s\n"%(p_i+1))
+    #       en_paragraph_multiline = "%N\n".join(paragraph.split("%N"))
+    #       newtip_out.write("%s\n"%(en_paragraph_multiline))
+    #       if not en_paragraph_multiline.endswith("\n"): newtip_out.write("\n")
+    #     newtip_out.flush()
+
 
   # Write modified lines
   with open(saveas, "wb") as f_out:
